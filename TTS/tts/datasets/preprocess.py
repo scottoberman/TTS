6f4c98a1966691f3ca4d18ee56e3f28f7c7160e1
Scott Oberman <scottoberman@gmail.com>
2022-01-16 13:01:42 -0800

    remove processing that want to handle later
    correct some settings set by og user

--- TTS/tts/datasets/preprocess.py
+++ TTS/tts/datasets/preprocess.py
@@ -338,3 +338,17 @@ def _voxcel_x(root_path, meta_file, voxcel_idx):
 
     with open(str(cache_to), 'r') as f:
         return [x.strip().split('|') for x in f.readlines()]
+
+
+def wendy(root_path, meta_file):
+    """Normalizes the Wendy meta data file to TTS format"""
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    speaker_name = "wendy"
+    with open(txt_file, 'r') as ttf:
+        for line in ttf:
+            cols = line.split('|')
+            wav_file = os.path.join(root_path, 'wavs', cols[0] + '.wav')
+            text = cols[1]
+            items.append([text, wav_file, speaker_name])
+    return items
